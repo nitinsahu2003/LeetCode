f9330119484abdabaac7234d2415c7eeea856615
Nitin Sahu <nitinsaahu00000@gmail.com>
2024-07-30 22:57:43 +0530

    Time: 8 ms (51.09%), Space: 40.3 MB (41.40%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,18 +1,9 @@
 class Solution {
     public int fib(int n) {
-        int sum=0;
-        int a=0;
-        int b=1;
         if(n<=1){
             return n;
         }
+        return fib(n-1)+fib(n-2);
 
-        for(int i=2;i<=n;i++){
-            sum=a+b;
-            a=b;
-            b=sum;
-            
-        }
-        return sum;
     }
 }
