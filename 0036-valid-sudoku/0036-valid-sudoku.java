16db9ba78bb10d132000000b7087a5e4f93eb44e
Nitin Sahu <nitinsaahu00000@gmail.com>
2024-08-06 13:27:42 +0530

    Time: 1 ms (100.00%), Space: 44.6 MB (42.76%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+       boolean[][] rows=new boolean[9][9];
+       boolean[][] cols=new boolean[9][9];
+       boolean[][] box=new boolean[9][9];
+
+       for(int i=0;i<9;i++){
+        for(int j=0;j<9;j++){
+
+            if(board[i][j]=='.'){
+                continue;
+            }
+
+            int num=board[i][j]-'1';
+
+            int gridIdx=(i/3)*3 + (j/3);
+
+            if(rows[i][num] || cols[j][num] || box[gridIdx][num]){
+                return false;
+            }
+
+            rows[i][num]=true;
+            cols[j][num]=true;
+            box[gridIdx][num]=true;
+        }
+       }
+       return true;
+    }
+}
