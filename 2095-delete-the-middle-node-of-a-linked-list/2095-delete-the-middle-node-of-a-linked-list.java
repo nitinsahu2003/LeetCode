875443811e546fca94cbfad69800358c8af4f57a
Nitin Sahu <nitinsaahu00000@gmail.com>
2024-08-17 12:43:12 +0530

    Time: 3 ms (99.72%), Space: 65.5 MB (12.75%) - LeetHub

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.java
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.java
@@ -13,19 +13,22 @@ class Solution {
         if(head==null||head.next==null){
             return null;
         }
-         int len=1,i=2;
-         ListNode start=head;
-         ListNode end=head;
-        while(end.next != null){
-            len++;
-            end=end.next;
+          ListNode slow = head;
+        ListNode fast = head;
+        ListNode prev = null;
+
+        // Move slow by 1 step and fast by 2 steps
+        while (fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
         }
-        int middle=len/2+1;
-        while(i<middle){
-            start=start.next;
-            i++;
+
+        // Delete the middle node
+        if (prev != null) {
+            prev.next = slow.next;
         }
-        start.next=start.next.next;
+
         return head;
     }
 }
