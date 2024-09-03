e2c51ae0f67da1a2491be8578b6beef9c2119f03
Nitin Sahu <nitinsaahu00000@gmail.com>
2024-09-03 08:20:48 +0530

    Time: 2 ms (76.19%), Space: 52.9 MB (52.67%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,18 +1,32 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        Map<Integer,Integer> map=new HashMap<>();
-        for(int num:nums){
-            map.put(num,map.getOrDefault(num,0)+1);
-        }
-        int maxFrequency=0;
-        int maxFrequencyElement=-1;
-        for(int key:map.keySet()){
-            int frequency=map.get(key);
-            if(frequency>maxFrequency){
-                maxFrequency=frequency;
-                maxFrequencyElement=key;
+        int result=0;
+        int count1=0, count2=0;
+        int elmnt1=0, elmnt2=1;
+        for(int i=0;i<nums.length;i++){
+            if(count1==0 && elmnt2!=nums[i]){
+                count1=1;
+                elmnt1=nums[i];
+            }else if(count2==0 && elmnt1!=nums[i]){
+                count2=1;
+                elmnt2=nums[i];
+            }else if(elmnt1==nums[i]){
+                count1++;
+            }else if(elmnt2==nums[i]){
+                count2++;
+            }else{
+                count1--;
+                count2--;
             }
         }
-        return maxFrequencyElement;
+        count1=0; count2=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==elmnt1) count1++;
+            if(nums[i]==elmnt2) count2++;
+        }
+        int min=(int)(nums.length/2)+1;
+        if(count1>=min) result= elmnt1;
+        if(count2>=min) result= elmnt2;
+        return result;
     }
 }
