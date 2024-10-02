8d5f7b7ba419f9a985c170cd25755495e0e7bf4c
Nitin Sahu <nitinsaahu00000@gmail.com>
2024-10-02 16:33:48 +0530

    Time: 0 ms (100.00%), Space: 46.1 MB (8.35%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int low=0, high=nums.length-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(target==nums[mid]) return mid;
+            if(target<nums[mid]) high=mid-1;
+            if(target>nums[mid]) low=mid+1;
+        }
+        return -1;
+    }
+}
