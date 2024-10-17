13b5a03a93ecbc24fac556ebeeb09ffa9075a14a
Nitin Sahu <nitinsaahu00000@gmail.com>
2024-10-17 11:27:31 +0530

    Time: 45 ms (60.91%), Space: 54.6 MB (65.25%) - LeetHub

--- 0493-reverse-pairs/0493-reverse-pairs.java
+++ 0493-reverse-pairs/0493-reverse-pairs.java
@@ -1,51 +1,40 @@
 class Solution {
     public int reversePairs(int[] nums) {
-       return mergeSort(nums,0,nums.length-1);
+        return mergeSortAndCount(nums,0,nums.length-1);
     }
-    private void merge(int[] nums, int low, int mid, int high){
-        List<Integer> ans=new ArrayList<>();
-        int left=low, right=mid+1;
-        while(left<=mid && right<=high){
-            if(nums[left]<=nums[right]){    //merge left half
-                ans.add(nums[left]);
-                left++;
-            }else{
-                ans.add(nums[right]);   //merge right half
-                right++;
-            }
-        }
-        // merge remaining elements
-        while(left<=mid){
-            ans.add(nums[left]);
-            left++;
-        }
-        while(right<=high){
-            ans.add(nums[right]);
-            right++;
-        }
-        for(int i=low;i<=high;i++){
-            nums[i]=ans.get(i-low);
-        }
-    }
-    private int mergeSort(int[] nums, int low ,int high){
+    private int mergeAndCount(int[] nums, int low, int mid, int high){
         int count=0;
-        if(low>=high) return count;
-        int mid=(low+high)/2;
-        count+=mergeSort(nums,low,mid);     //sorting left half
-        count+=mergeSort(nums,mid+1,high);  //sorting right half
-        count+=countPairs(nums,low,mid,high);   //adding number of pairs to result
-        merge(nums,low,mid,high);   //merging the halves
-        return count;
-    }
-    private int countPairs(int[] nums, int low, int mid, int high){
-         int right=mid+1;
-         int count=0;
-         for(int i=low;i<=mid;i++){
-             while(right<=high && (long)nums[i]>2L*nums[right]){
+        int right=mid+1;
+        for(int i=low;i<=mid;i++){
+            while(right<=high && (long)nums[i]>2*(long)nums[right]){
                 right++;
             }
             count+=(right-(mid+1));
         }
-         return count;
+        int[] temp=new int[high-low+1];
+        int k=0, left=low, r=mid+1;
+        while(left<=mid && r<=high){
+            if(nums[left]<=nums[r]) temp[k++]=nums[left++];
+            else temp[k++]=nums[r++];
+        }
+        while(left<=mid){
+            temp[k++]=nums[left++];
+        } 
+        while(r<=high){
+            temp[k++]=nums[r++];
+        } 
+        for(int i=low;i<=high;i++){
+           nums[i]=temp[i-low];
+        }
+        return count;
+    }
+    private int mergeSortAndCount(int[] nums, int low, int high){
+        int count=0;
+        if(low>=high) return count;
+        int mid=(low+high)/2;
+        count+=mergeSortAndCount(nums,low,mid);
+        count+=mergeSortAndCount(nums,mid+1,high);
+        count+=mergeAndCount(nums,low,mid,high);
+        return count;
     }
 }
