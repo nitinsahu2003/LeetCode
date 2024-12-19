803bfb7e24a190278ea1d319bb8f8393fae204e5
Nitin Sahu <nitinsaahu00000@gmail.com>
2024-12-19 12:25:34 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (86.23%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,15 +1,18 @@
 class Solution {
     public int rob(int[] nums) {
-        if(nums.length==1) return nums[0]; //array with single element
+        if(nums.length==1) return nums[0];
         else if(nums.length==2) return Math.max(nums[0],nums[1]);
-        else return Math.max(helper(nums,0,nums.length-2),helper(nums,1,nums.length-1)); //rob house excluding the first house or last house
+        else return Math.max(helper(nums, 0 , nums.length-2), helper(nums, 1, nums.length-1));
     }
-    private int helper(int[] nums,int start, int end){
-        int prev1=0, prev2=0;
-        for(int i=start;i<=end;i++){
-            int curr=Math.max(prev1,prev2+nums[i]);
-            prev2=prev1;
-            prev1=curr;
+
+    private int helper(int[] nums, int start, int end) {
+        int prev1 = 0, prev2 = 0;
+        for (int i = start; i <=end; i++) {
+            int pick = nums[i] + prev2;
+            int nonPick = prev1;
+            int curr = Math.max(pick, nonPick);
+            prev2 = prev1;
+            prev1 = curr;
         }
         return prev1;
     }
