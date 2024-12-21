2e682d87c8e8e10bece6d775761ba7d33f10f89e
Nitin Sahu <nitinsaahu00000@gmail.com>
2024-12-21 12:36:21 +0530

    Update 0063-unique-paths-ii.java

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -21,62 +21,62 @@ class Solution {
     }
 
     // 2. TABULATION APPROACH - 1ms time complexity
-//     public int uniquePathsWithObstacles(int[][] maze){
-//         int m = maze.length;
-//         int n = maze[0].length;
-//         int[][] dp = new int[m][n];
-//         for(int[] row : dp){
-//             Arrays.fill(row, -1);
-//         }
-//         return helper(m, n, dp, maze);
-//     }
-//     public int helper(int m, int n, int[][] dp, int[][] maze){
-//         for(int i=0;i<m;i++){
-//             for(int j=0;j<n;j++){
-//                 if(i >= 0 && j >= 0 && maze[i][j] == 1){
-//                     dp[i][j] = 0;
-//                     continue;
-//                 }
-//                 if(i == 0 && j == 0){
-//                     dp[i][j] = 1;
-//                     continue;
-//                 }
-//                 int up = 0, left = 0;
-//                 if(i>0) up = dp[i-1][j];
-//                 if(j>0) left = dp[i][j-1];
+     public int uniquePathsWithObstacles(int[][] maze){
+         int m = maze.length;
+         int n = maze[0].length;
+         int[][] dp = new int[m][n];
+         for(int[] row : dp){
+             Arrays.fill(row, -1);
+         }
+         return helper(m, n, dp, maze);
+     }
+    public int helper(int m, int n, int[][] dp, int[][] maze){
+         for(int i=0;i<m;i++){
+             for(int j=0;j<n;j++){
+                 if(i >= 0 && j >= 0 && maze[i][j] == 1){
+                     dp[i][j] = 0;
+                     continue;
+                 }
+                 if(i == 0 && j == 0){
+                     dp[i][j] = 1;
+                     continue;
+                 }
+                 int up = 0, left = 0;
+                 if(i>0) up = dp[i-1][j];
+                 if(j>0) left = dp[i][j-1];
 
-//                 dp[i][j] = up + left;  
-//             }
-//         }
-//         return dp[m-1][n-1];
-//     }
+                 dp[i][j] = up + left;  
+             }
+         }
+         return dp[m-1][n-1];
+     }
 
     // 3. Space optimization approach
-//     public int uniquePathsWithObstacles(int[][] maze){
-//         int m = maze.length;
-//         int n = maze[0].length;
-//         int[] prev = new int[m];
+     public int uniquePathsWithObstacles(int[][] maze){
+         int m = maze.length;
+         int n = maze[0].length;
+         int[] prev = new int[m];
 
-//         for(int i=0;i<m;i++){
-//             int[] temp = new int[n];
-//             for(int j=0;j<n;j++){
-//                 if(i >= 0 && j>= 0 && maze[i][j] == 1){
-//                     temp[j] = 0;
-//                     continue;
-//                 }
-//                 if(i == 0 && j == 0){
-//                     temp[j] = 1;
-//                     continue;
-//                 }
+         for(int i=0;i<m;i++){
+             int[] temp = new int[n];
+             for(int j=0;j<n;j++){
+                 if(i >= 0 && j>= 0 && maze[i][j] == 1){
+                     temp[j] = 0;
+                     continue;
+                 }
+                 if(i == 0 && j == 0){
+                     temp[j] = 1;
+                     continue;
+                 }
 
-//                 int up = 0, left = 0;
-//                 if(i > 0) up = prev[j];
-//                 if(j > 0) left = temp[j-1];
+                 int up = 0, left = 0;
+                 if(i > 0) up = prev[j];
+                 if(j > 0) left = temp[j-1];
                  
-//                 temp[j] = up + left;
-//             }
-//             prev = temp;
-//         }
-//         return prev[n-1];
-//     }
+                 temp[j] = up + left;
+             }
+             prev = temp;
+         }
+         return prev[n-1];
+     }
 }
