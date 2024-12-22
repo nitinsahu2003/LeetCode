be599892c2a7500d858e39d30aa22d2c86563f96
Nitin Sahu <nitinsaahu00000@gmail.com>
2024-12-22 18:36:56 +0530

    Time: 1 ms (99.66%), Space: 44.8 MB (16.80%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,15 +1,23 @@
 class Solution {
+    private int[][] dp;
+
+    // 1. Memoization approach 
     public int minimumTotal(List<List<Integer>> triangle) {
-        int row=triangle.size();
-        int[] dp=new int[row];
-        for(int i=0;i<row;i++){
-            dp[i]=triangle.get(row-1).get(i);
+        int n = triangle.size();
+        dp = new int[n][n];
+        for(int[] row : dp){
+            Arrays.fill(row,-1);
         }
-        for(int i=row-2;i>=0;i--){
-            for(int j=0;j<=i;j++){
-                dp[j]=Math.min(dp[j],dp[j+1])+triangle.get(i).get(j);
-            }
-        }
-        return dp[0];
+        return helper(0, 0, n, triangle);
+    }
+    private int helper(int i, int j, int n, List<List<Integer>> triangle){
+        if(i == n-1) return triangle.get(i).get(j);
+        if(dp[i][j] != -1) return dp[i][j];
+
+        int down = helper(i+1, j, n, triangle);
+        int diagonal = helper(i+1, j+1, n, triangle);
+
+        dp[i][j] = triangle.get(i).get(j) + Math.min(down, diagonal);
+        return dp[i][j];
     }
 }
