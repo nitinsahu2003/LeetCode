b65771f6512d01598c1d297e4494971aa53e2eae
Nitin Sahu <nitinsaahu00000@gmail.com>
2024-12-22 15:13:25 +0530

    Update 0064-minimum-path-sum.java

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -22,55 +22,55 @@ class Solution {
     }
 
     // 2. TABULATION APPROACH
-    // public int minPathSum(int[][] grid){
-    //     int m = grid.length;
-    //     int n = grid[0].length;
-    //     int[][] dp = new int[m][n];
-    //     for(int i=0;i<m;i++){
-    //         for(int j=0;j<n;j++){
-    //             if(i==0 && j==0){
-    //                 dp[i][j] = grid[0][0];
-    //             }
-    //             else{
-    //                 int up = grid[i][j];
-    //                 if(i>0) up += dp[i-1][j];
-    //                 else up += (int) Math.pow(10,9);
+    public int minPathSum(int[][] grid){
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] dp = new int[m][n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0){
+                    dp[i][j] = grid[0][0];
+                }
+                else{
+                    int up = grid[i][j];
+                    if(i>0) up += dp[i-1][j];
+                    else up += (int) Math.pow(10,9);
 
-    //                 int left = grid[i][j];
-    //                 if(j>0) left += dp[i][j-1];
-    //                 else left += (int) Math.pow(10,9);
+                    int left = grid[i][j];
+                    if(j>0) left += dp[i][j-1];
+                    else left += (int) Math.pow(10,9);
 
-    //                 dp[i][j] = Math.min(up,left);
-    //             }
-    //         }
-    //     }
-    //     return dp[m-1][n-1];
-    // }
+                    dp[i][j] = Math.min(up,left);
+                }
+            }
+        }
+        return dp[m-1][n-1];
+    }
 
     // 3. SPACE OPTIMIZATION APPROACH
-//     public int minPathSum(int[][] grid){
-//         int m = grid.length;
-//         int n = grid[0].length;
-//         int[] prev = new int[n];
+    public int minPathSum(int[][] grid){
+        int m = grid.length;
+        int n = grid[0].length;
+        int[] prev = new int[n];
 
-//         for(int i=0;i<m;i++){
-//             int[] temp = new int[n];
-//             for(int j=0;j<n;j++){
-//                 if(i==0 && j==0) temp[j] = grid[i][j];
-//                 else{
-//                     int up = grid[i][j];
-//                     if(i>0) up += prev[j];
-//                     else up += (int) Math.pow(10,9);
+        for(int i=0;i<m;i++){
+            int[] temp = new int[n];
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0) temp[j] = grid[i][j];
+                else{
+                    int up = grid[i][j];
+                    if(i>0) up += prev[j];
+                    else up += (int) Math.pow(10,9);
 
-//                     int left = grid[i][j];
-//                     if(j>0) left += temp[j-1];
-//                     else left += (int) Math.pow(10,9);
+                    int left = grid[i][j];
+                    if(j>0) left += temp[j-1];
+                    else left += (int) Math.pow(10,9);
 
-//                     temp[j] = Math.min(up,left);
-//                 }
-//             }
-//             prev = temp;
-//         }
-//         return prev[n-1];
-//     }
+                    temp[j] = Math.min(up,left);
+                }
+            }
+            prev = temp;
+        }
+        return prev[n-1];
+    }
 }
