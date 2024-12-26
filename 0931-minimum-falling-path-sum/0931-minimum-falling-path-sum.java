921dd6ca5743f716e8c9fa6eb2ebb156ec370561
Nitin Sahu <nitinsaahu00000@gmail.com>
2024-12-26 19:41:40 +0530

    Update 0931-minimum-falling-path-sum.java

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
@@ -3,65 +3,65 @@ class Solution {
     // 1. MEMOIZATION APPROACH
     // Time Complexity: O(M*N)
     //Space Complexity: O(N) + O(N*M)
-    // public int minFallingPathSum(int[][] matrix) {
-    //     int m = matrix.length;
-    //     int n = matrix[0].length;
-    //     int[][] dp = new int[m][n];
-    //     for(int[] row : dp){
-    //         Arrays.fill(row, -1);
-    //     }
-    //     int min = Integer.MAX_VALUE;
-    //     for(int j=0;j<n;j++){
-    //         int ans = helper(m-1, j, m, n, dp, matrix);
-    //         min = Math.min(min, ans);
-    //     }
-    //     return min;
-    // }
-    // public int helper(int i, int j, int m, int n, int[][] dp, int[][] matrix){
-    //     if(j<0 || j>=n) return (int) Math.pow(10,9);
-    //     if(i==0) return matrix[0][j];
-    //     if(dp[i][j] != -1) return dp[i][j];
+     public int minFallingPathSum(int[][] matrix) {
+         int m = matrix.length;
+         int n = matrix[0].length;
+         int[][] dp = new int[m][n];
+         for(int[] row : dp){
+             Arrays.fill(row, -1);
+         }
+         int min = Integer.MAX_VALUE;
+         for(int j=0;j<n;j++){
+             int ans = helper(m-1, j, m, n, dp, matrix);
+             min = Math.min(min, ans);
+         }
+         return min;
+     }
+     public int helper(int i, int j, int m, int n, int[][] dp, int[][] matrix){
+         if(j<0 || j>=n) return (int) Math.pow(10,9);
+         if(i==0) return matrix[0][j];
+         if(dp[i][j] != -1) return dp[i][j];
 
-    //     int up = matrix[i][j] + helper(i-1, j, m, n, dp, matrix);
-    //     int left = matrix[i][j] + helper(i-1, j-1, m, n, dp, matrix);
-    //     int right = matrix[i][j] + helper(i-1, j+1, m, n, dp, matrix);
+         int up = matrix[i][j] + helper(i-1, j, m, n, dp, matrix);
+         int left = matrix[i][j] + helper(i-1, j-1, m, n, dp, matrix);
+         int right = matrix[i][j] + helper(i-1, j+1, m, n, dp, matrix);
 
-    //     return dp[i][j] = Math.min(up, Math.min(left, right));
-    // }
+         return dp[i][j] = Math.min(up, Math.min(left, right));
+     }
 
     // 2. TABULATION APPROACH
     // Time Complexity: O(N*M)
     // Space Complexity: O(N*M)
-    // public int minFallingPathSum(int[][] matrix){
-    //     int m = matrix.length;
-    //     int n = matrix[0].length;
+     public int minFallingPathSum(int[][] matrix){
+         int m = matrix.length;
+         int n = matrix[0].length;
 
-    //     int[][] dp = new int[m][n];
+         int[][] dp = new int[m][n];
 
-    //     for(int j=0;j<n;j++){
-    //         dp[0][j] = matrix[0][j];
-    //     }
-    //     for(int i=1;i<m;i++){
-    //         for(int j=0;j<n;j++){
-    //             int up = matrix[i][j] + dp[i-1][j];
+         for(int j=0;j<n;j++){
+             dp[0][j] = matrix[0][j];
+         }
+         for(int i=1;i<m;i++){
+             for(int j=0;j<n;j++){
+                 int up = matrix[i][j] + dp[i-1][j];
 
-    //             int left = matrix[i][j];
-    //             if(j-1 >= 0) left += dp[i-1][j-1];
-    //             else left += (int) Math.pow(10,9);
+                 int left = matrix[i][j];
+                 if(j-1 >= 0) left += dp[i-1][j-1];
+                 else left += (int) Math.pow(10,9);
 
-    //             int right = matrix[i][j];
-    //             if(j+1 < n) right += dp[i-1][j+1];
-    //             else right += (int) Math.pow(10,9);
+                 int right = matrix[i][j];
+                 if(j+1 < n) right += dp[i-1][j+1];
+                 else right += (int) Math.pow(10,9);
 
-    //             dp[i][j] = Math.min(up, Math.min(left, right));
-    //         } 
-    //     }
-    //     int min = Integer.MAX_VALUE;
-    //     for(int j=0;j<n;j++){
-    //         min = Math.min(min, dp[m-1][j]);
-    //     }
-    //     return min;
-    // }
+                 dp[i][j] = Math.min(up, Math.min(left, right));
+             } 
+         }
+         int min = Integer.MAX_VALUE;
+          for(int j=0;j<n;j++){
+             min = Math.min(min, dp[m-1][j]);
+         }
+         return min;
+     }
 
     // 3. Space optimization approach
     // Time Complexity: O(N*M)
