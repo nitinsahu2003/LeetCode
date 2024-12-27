45b1c1de04de3b28a8ee3dccf98d9df7f4556bbe
Nitin Sahu <nitinsaahu00000@gmail.com>
2024-12-27 12:00:32 +0530

    Time: 39 ms (65.49%), Space: 45.3 MB (75.12%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,18 +1,94 @@
 class Solution {
-    public boolean canPartition(int[] nums) {
+    // 1. MEMOIZATION APPROACH
+    // Time Complexity: O(N*K) + O(N)
+    // Space Complexity: O(N*K) + O(N)
+    // public boolean canPartition(int[] nums) {
+    //     int sum = 0;
+    //     int n = nums.length;
+    //     for(int i=0;i<n;i++){
+    //         sum += nums[i];
+    //     }
+    //     if(sum % 2 ==1) return false;
+    //         int k = sum / 2;
+    //         int[][] dp = new int[n][k+1];
+    //         for(int[] row : dp){
+    //             Arrays.fill(row, -1);
+    //     }
+    //     return helper(n-1, k, dp, nums);
+    // }
+    // public boolean helper(int ind, int target, int[][] dp, int[] nums){
+    //     if(target==0) return true;
+    //     if(ind==0) return nums[ind] == target;
+    //     if(dp[ind][target] != -1){
+    //         if(dp[ind][target] == 0) return false;
+    //         else return true;
+    //     }
+
+    //     boolean nonPick = helper(ind-1, target, dp, nums);
+    //     boolean pick = false;
+    //     if(nums[ind] <= target) pick = helper(ind-1, target - nums[ind], dp, nums);
+
+    //     if(pick || nonPick){
+    //         dp[ind][target] = 1;
+    //         return true;
+    //     }else{
+    //         dp[ind][target] = 0;
+    //         return false;
+    //     }
+    // }
+
+    // 2. TABULATION APPROACH
+    // Time Complexity: O(N*K) + O(N)
+    // Space Complexity: O(N*K) 
+    // public boolean canPartition(int[] nums){
+    //     int n = nums.length;
+    //     int sum = 0;
+    //     for(int i=0;i<n;i++){
+    //         sum += nums[i];
+    //     }
+    //     if(sum % 2 == 1) return false;
+    //     int k = sum / 2;
+    //     boolean[][] dp = new boolean[n][k+1];
+    //     for(int i=0;i<n;i++){
+    //         dp[i][0] = true;
+    //     }
+    //     if(nums[0] <= k) dp[0][nums[0]] = true;
+    //     for(int i=1;i<n;i++){
+    //         for(int target=1; target<=k;target++){
+    //             boolean notPick = dp[i-1][target];
+    //             boolean pick = false;
+    //             if(nums[i]<=target) pick = dp[i-1][target - nums[i]];
+
+    //             dp[i][target] = pick || notPick;
+    //         }
+    //     }
+    //     return dp[n-1][k];
+    // }
+
+    // 3. SPACE OPTIMIZATION APPROACH
+    public boolean canPartition(int[] nums){
+        int n =nums.length;
         int sum = 0;
-        for(int i=0;i<nums.length;i++){
-            sum+=nums[i];
+        for(int i=0;i<n;i++){
+            sum += nums[i];
         }
-        if(sum % 2 != 0) return false;
-        int target = sum / 2;
-        boolean[] dp = new boolean[target + 1];
-        dp[0] = true;
-        for(int num : nums){
-            for(int j = target ; j >= num ; j--){
-                dp[j] = dp[j] || dp[j-num];
+        if(sum % 2 == 1) return false;
+        int k = sum / 2;
+        boolean[] prev = new boolean[k+1];
+        prev[0] = true;
+        if(nums[0] <= k) prev[nums[0]] = true;
+        for(int i=1;i<n;i++){
+            boolean[] curr = new boolean[k+1];
+            curr[0] = true;
+            for(int target=1;target<=k;target++){
+                boolean notPick = prev[target];
+                boolean pick = false;
+                if(nums[i] <= target) pick = prev[target-nums[i]];
+
+                curr[target] = pick || notPick;
             }
+            prev = curr;
         }
-        return dp[target];
+        return prev[k];
     }
 }
