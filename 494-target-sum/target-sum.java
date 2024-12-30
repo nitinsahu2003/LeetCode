5d1980eb56217d528e1541adcd2f95505f45e21a
Nitin Sahu <nitinsaahu00000@gmail.com>
2024-12-30 10:52:16 +0530

    Time: 4 ms (93.52%) | Memory: 43.4 MB (40.19%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,30 +1,32 @@
 class Solution {
-
-    //static int mod = (int) (Math.pow(10,9)+7);
     public int findTargetSumWays(int[] nums, int target){
         int sum = 0;
         for(int num : nums){
             sum += num;
         }
-        if((sum-target)<0 || (sum - target) %2==1) return 0;
+        if((sum - target) < 0 || (sum - target) % 2 == 1) return 0;
         return helper(nums, (sum - target) / 2);
     }
     public int helper(int[] nums, int tar){
         int n = nums.length;
-        int[][] dp = new int[n][tar+1];
-        if(nums[0] == 0) dp[0][0] = 2;
-        else dp[0][0] = 1;
+        int[] prev = new int[tar+1];
 
-        if(nums[0] != 0 && nums[0] <= tar) dp[0][nums[0]] = 1;
+        if(nums[0] == 0) prev[0] = 2;
+        else prev[0] = 1;
+
+        if(nums[0] != 0 && nums[0] <= tar) prev[nums[0]] = 1;
+
         for(int i=1;i<n;i++){
+            int[] curr = new int[tar+1];
             for(int target=0;target<=tar;target++){
-                int notPick = dp[i-1][target];
+                int notPick = prev[target];
                 int pick = 0;
-                 if(nums[i] <= target) pick = dp[i-1][target-nums[i]];
+                if(nums[i] <= target) pick = prev[target - nums[i]];
 
-                 dp[i][target] = (pick + notPick); //% mod;
+                curr[target] = (pick + notPick);
             }
+            prev = curr;
         }
-        return dp[n-1][tar];
-    }
+        return prev[tar];
+    } 
 }
