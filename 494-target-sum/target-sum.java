2487598b462d5e6009141a77186ba6b1f47bad85
Nitin Sahu <nitinsaahu00000@gmail.com>
2024-12-30 10:39:28 +0530

    Time: 5 ms (90.55%) | Memory: 43.1 MB (42.14%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,30 +1,29 @@
 class Solution {
-    public int findTargetSumWays(int[] nums, int target) {
-        int n = nums.length;
+    static int mod = (int) (Math.pow(10,9)+7);
+    public int findTargetSumWays(int[] nums, int target){
         int sum = 0;
-        for(int i=0;i<n;i++){
-            sum += nums[i];
+        for(int num : nums){
+            sum += num;
         }
-        if((sum - target) < 0) return 0;
-        if((sum - target) % 2 == 1) return 0;
-        int s2 = (sum - target) / 2;
-        int[][] dp = new int[n][s2+1];
-        for(int[] row : dp){
-            Arrays.fill(row, -1);
-        }
-        return helper(n-1, s2, nums, dp);
+        if((sum-target)<0 || (sum - target) %2==1) return 0;
+        return helper(nums, (sum - target) / 2);
     }
-    public int helper(int i, int target, int[] nums, int[][] dp){
-        if(i == 0){
-            if(target == 0 && nums[0] == 0) return 2;
-            if(target == 0 || target == nums[0]) return 1;
-            else return 0;
+    public int helper(int[] nums, int tar){
+        int n = nums.length;
+        int[][] dp = new int[n][tar+1];
+        if(nums[0] == 0) dp[0][0] = 2;
+        else dp[0][0] = 1;
+
+        if(nums[0] != 0 && nums[0] <= tar) dp[0][nums[0]] = 1;
+        for(int i=1;i<n;i++){
+            for(int target=0;target<=tar;target++){
+                int notPick = dp[i-1][target];
+                int pick = 0;
+                 if(nums[i] <= target) pick = dp[i-1][target-nums[i]];
+
+                 dp[i][target] = (pick + notPick) % mod;
+            }
         }
-        if(dp[i][target] != -1) return dp[i][target];
-        int notPick = helper(i-1, target, nums, dp);
-        int pick = 0;
-        if(nums[i] <= target) pick = helper(i-1, target - nums[i], nums, dp);
-        
-        return dp[i][target] = (pick + notPick);
+        return dp[n-1][tar];
     }
 }
