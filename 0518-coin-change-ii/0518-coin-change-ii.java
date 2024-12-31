420ee7c9e012c67f81b9dbfdeccfe96b497564f7
Nitin Sahu <nitinsaahu00000@gmail.com>
2024-12-31 11:37:28 +0530

    Time: 9 ms (63.71%), Space: 49.8 MB (16.25%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,21 +1,21 @@
 class Solution {
     public int change(int amount, int[] coins) {
         int n = coins.length;
-        int[][] dp = new int[n][amount+1];
-        for(int[] row : dp){
-            Arrays.fill(row, -1);
+        
+        int[][] dp = new int[n][amount + 1];
+
+        for(int i=0;i<=amount;i++){
+            if(i % coins[0] == 0) dp[0][i] = 1;
         }
-        return helper(n-1, amount, coins, dp);
-    }
-    public int helper(int i, int amount, int[] coins, int[][] dp){
-        if(i == 0){
-            if(amount % coins[0] == 0) return 1;
-            else return 0;
+
+        for(int i=1;i<n;i++){
+            for(int target=0;target<=amount;target++){
+                int notPick = dp[i-1][target];
+                int pick = 0;
+                if(coins[i] <= target) pick = dp[i][target - coins[i]];
+                dp[i][target] = pick + notPick;
+            }
         }
-        if(dp[i][amount] != -1) return dp[i][amount];
-        int notPick = helper(i-1, amount, coins, dp);
-        int pick = 0;
-        if(coins[i] <= amount) pick = helper(i, amount - coins[i], coins, dp);
-        return dp[i][amount] = pick + notPick;
+        return dp[n-1][amount];
     }
 }
