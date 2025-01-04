cc295c9d45fa1bead078267b3339a40ba2bb9fa9
Nitin Sahu <nitinsaahu00000@gmail.com>
2025-01-04 10:53:32 +0530

    Time: 54 ms (45.57%), Space: 45.4 MB (93.71%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
@@ -6,24 +6,16 @@ class Solution {
     public int helper(String s1, String s2){
         int m = s1.length();
         int n = s2.length();
+        int[] prev = new int[n+1];
+        int[] curr = new int[n+1];
 
-        int[][] dp = new int[m+1][n+1];
-        for(int[] row: dp){
-            Arrays.fill(row, -1);
-        }
-        for(int i=0;i<m;i++){
-            dp[i][0] = 0;
-        }
-        for(int i=0;i<n;i++){
-            dp[0][i] = 0;
-        }
-
-        for(int i1=1;i1<=m;i1++){
-            for(int i2=1;i2<=n;i2++){
-                if(s1.charAt(i1-1) == s2.charAt(i2-1)) dp[i1][i2] = 1 + dp[i1-1][i2-1];
-                else dp[i1][i2] = Math.max(dp[i1][i2-1], dp[i1-1][i2]);
+        for(int ind1=1;ind1<=m;ind1++){
+            for(int ind2=1;ind2<=n;ind2++){
+                if(s1.charAt(ind1-1) == s2.charAt(ind2-1)) curr[ind2] = 1 + prev[ind2-1];
+                else curr[ind2] = Math.max(prev[ind2], curr[ind2-1]);
             }
+            prev = curr.clone();
         }
-        return dp[m][n];
+        return prev[n];
     }
 }
