9f0530fcfcf628ced20bad47ac6731c13ce58c35
Nitin Sahu <nitinsaahu00000@gmail.com>
2025-01-05 21:24:02 +0530

    Time: 26 ms (59%), Space: 45.4 MB (71.73%) - LeetHub

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/1437-minimum-insertion-steps-to-make-a-string-palindrome.java
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/1437-minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -6,25 +6,16 @@ class Solution {
     public int helper(String s1, String s2){
         int m = s1.length();
         int n = s2.length();
-
-        int[][] dp = new int[m+1][n+1];
-        for(int[] row : dp){
-            Arrays.fill(row, -1);
-        }
-
-        for(int i=0;i<=m;i++){
-            dp[i][0] = 0;
-        }
-        for(int i=0;i<=n;i++){
-            dp[0][i] = 0;
-        }
+        int[] prev = new int[m+1];
+        int[] curr = new int[n+1];
 
         for(int ind1=1;ind1<=m;ind1++){
             for(int ind2=1;ind2<=n;ind2++){
-                if(s1.charAt(ind1-1) == s2.charAt(ind2-1)) dp[ind1][ind2] = 1 + dp[ind1-1][ind2-1];
-                else dp[ind1][ind2] = Math.max(dp[ind1-1][ind2], dp[ind1][ind2-1]);
+                if(s1.charAt(ind1-1) == s2.charAt(ind2-1)) curr[ind2] = 1 + prev[ind2-1];
+                else curr[ind2] = Math.max(prev[ind2], curr[ind2-1]);
             }
+            prev = curr.clone();
         }
-        return dp[m][n];
+        return prev[m];
     }
-}
+}   
