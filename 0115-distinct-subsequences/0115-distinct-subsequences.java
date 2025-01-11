86bd64ac0d0c1305bd3edf05018ae95664d59019
Nitin Sahu <nitinsaahu00000@gmail.com>
2025-01-11 10:51:12 +0530

    Time: 19 ms (60.17%), Space: 51.1 MB (57.78%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -3,22 +3,25 @@ class Solution {
         int m = s1.length();
         int n = s2.length();
         int[][] dp = new int[m+1][n+1];
-        for(int[] row : dp){
-            Arrays.fill(row, -1);
-        }
-        return helper(s1, s2, m, n, dp);
-    }
-    public int helper(String s1, String s2, int i, int j, int[][] dp){
-        if(j==0) return 1;
-        if(i==0) return 0;
-        if(dp[i][j] != -1) return dp[i][j];
 
-        if(s1.charAt(i-1) == s2.charAt(j-1)){
-            int notPick = helper(s1, s2, i-1, j-1, dp);
-            int pick = helper(s1, s2, i-1, j, dp);
-            return dp[i][j] = pick + notPick;
-        }else{
-            return dp[i][j] = helper(s1, s2, i-1, j, dp);
-        }     
+        for(int i=0;i<=m;i++){
+            dp[i][0] = 1;
+        }
+        for(int i=1;i<=n;i++){
+            dp[0][i] = 0;
+        }
+
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(s1.charAt(i-1) == s2.charAt(j-1)){
+                    int notPick = dp[i-1][j-1];
+                    int pick = dp[i-1][j];
+                    dp[i][j] = pick + notPick;
+                }else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        return dp[m][n];
     }
 }
