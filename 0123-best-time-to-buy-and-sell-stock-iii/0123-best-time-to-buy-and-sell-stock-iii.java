f4fe9e9999da8e3d2ea8e61c0ff9e51ce9f3f2db
Nitin Sahu <nitinsaahu00000@gmail.com>
2025-01-16 09:53:11 +0530

    Time: 61 ms (37.07%), Space: 62.5 MB (40.22%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -1,30 +1,25 @@
 class Solution {
+    //2. Tabulation Approach
     public int maxProfit(int[] prices) {
         int n = prices.length;
-        int[][][] dp = new int[n][2][3];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<=1;j++){
-                Arrays.fill(dp[i][j], -1);
+        int[][][] dp = new int[n+1][2][3];
+        
+        int profit = 0;
+        for(int ind=n-1;ind>=0;ind--){
+            for(int buy=0;buy<=1;buy++){
+                for(int cap=1;cap<=2;cap++){
+                    if(buy == 0){
+                        dp[ind][buy][cap] = Math.max(0 + dp[ind+1][0][cap],
+                        -prices[ind] + dp[ind+1][1][cap]);
+                    }
+                    if(buy == 1){
+                        dp[ind][buy][cap] = Math.max(0 + dp[ind+1][1][cap],
+                        prices[ind] + dp[ind+1][0][cap-1]);
+                    }
+                }
             }
         }
-        return helper(prices, 0, 0, 2, dp);
-
+        return dp[0][0][2];
     }
-    public int helper(int[] prices, int ind, int buy, int cap, int[][][] dp){
-        int n = prices.length;
-        if(ind == n || cap == 0) return 0;
 
-        if(dp[ind][buy][cap] != -1) return dp[ind][buy][cap];
-        int profit = 0;
-
-        if(buy==0){
-            profit = Math.max(0 + helper(prices, ind+1, 0, cap, dp),
-                    -prices[ind] + helper(prices, ind+1, 1, cap, dp));
-        }
-        else{
-            profit = Math.max(0 + helper(prices, ind+1, 1, cap, dp),
-                    prices[ind] + helper(prices, ind+1, 0, cap - 1, dp));
-        }
-        return dp[ind][buy][cap] = profit;
-    }
 }
