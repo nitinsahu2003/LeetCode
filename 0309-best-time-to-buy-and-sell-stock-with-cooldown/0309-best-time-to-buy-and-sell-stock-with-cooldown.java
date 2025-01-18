964f3297079eb13ccb617daef2f958aaee4b5f5b
Nitin Sahu <nitinsaahu00000@gmail.com>
2025-01-18 13:05:33 +0530

    Time: 1 ms (80.72%), Space: 42.2 MB (19.33%) - LeetHub

---
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        int[][] dp = new int[n][2];
+
+        for(int[] row : dp){
+            Arrays.fill(row, -1);
+        }
+        return helper(prices, 0, 0, dp);
+    }
+    public int helper(int[] prices, int ind, int buy, int[][] dp){
+        int n = prices.length;
+        if(ind >= n) return 0;
+        if(dp[ind][buy] != -1) return dp[ind][buy];
+        int profit = 0;
+        if(buy == 0){
+            profit = Math.max(0 + helper(prices, ind+1, 0, dp),
+            -prices[ind] + helper(prices, ind+1, 1, dp));
+        }
+        if(buy == 1){
+            profit = Math.max(helper(prices, ind+1, 1, dp),
+            prices[ind] + helper(prices, ind+2, 0, dp)); // +2 is because you can't buy next day after selling the stock.
+        }
+        return dp[ind][buy] = profit;
+    }
+}
