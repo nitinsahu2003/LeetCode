d60342101e7f4dcb1fe083d571fd9c831a2d944e
Nitin Sahu <nitinsaahu00000@gmail.com>
2025-01-18 13:12:28 +0530

    Time: 1 ms (80.72%), Space: 41.7 MB (77.11%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -3,24 +3,17 @@ class Solution {
         int n = prices.length;
         int[][] dp = new int[n+2][2];
 
-        for(int[] row : dp){
-            Arrays.fill(row, -1);
+        for(int ind=n-1;ind>=0;ind--){
+            for(int buy=0;buy<=1;buy++){
+                if(buy==0){
+                    dp[ind][buy] = Math.max(0 + dp[ind+1][0],
+                    -prices[ind] + dp[ind+1][1]);
+                }else{
+                    dp[ind][buy] = Math.max(0 + dp[ind+1][1],
+                    prices[ind] + dp[ind+2][0]);
+                }
+            }
         }
-        return helper(prices, 0, 0, dp);
-    }
-    public int helper(int[] prices, int ind, int buy, int[][] dp){
-        int n = prices.length;
-        if(ind >= n) return 0;
-        if(dp[ind][buy] != -1) return dp[ind][buy];
-        int profit = 0;
-        if(buy == 0){
-            profit = Math.max(0 + helper(prices, ind+1, 0, dp),
-            -prices[ind] + helper(prices, ind+1, 1, dp));
-        }
-        if(buy == 1){
-            profit = Math.max(helper(prices, ind+1, 1, dp),
-            prices[ind] + helper(prices, ind+2, 0, dp)); // +2 is because you can't buy next day after selling the stock.
-        }
-        return dp[ind][buy] = profit;
+        return dp[0][0];
     }
 }
